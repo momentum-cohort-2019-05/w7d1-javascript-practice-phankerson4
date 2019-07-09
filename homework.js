aaff5a5d1835afacd0c9d72d5f05d698694cc415
phankerson4 <phankerson4@gmail.com>
2019-07-09 10:28:33 -0400

    Completed thru Q12

--- homework.js
+++ homework.js
@@ -1,26 +1,66 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = "Addison"
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20
+let people = 6
+leftover = 20 % 6
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+function greeting(name) {
+    return `Hello, ${name}!`
+  }
+  
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+
+function isOdd(num) {
+    if(num % 2 === 1|| num % 2 === -1) {
+        return true
+    } else {
+        return false
+}
+}
+
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+
+function isEven(num) {
+    if(num % 2 === 0 ) {
+        return true
+    } else {
+        return false
+}
+}
+
+
+
+
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(f) {
+    const Celsius = (f-32) * 5/9
+    return Celsius
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(c) {
+    const Fahrenheit = (c *9/5) + 32
+    return Fahrenheit
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +69,22 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(f) {
+    const Kelvin = (f-32)* 5/9 + 273.15
+    return Kelvin
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser(a,b) {
+    if (a < b) {
+    return a 
+}else{
+    return b
+}
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -45,19 +98,70 @@
 //
 // If any other language code is used, return nothing.
 
+function multigreeting(name, language) { 
+
+    
+    if (language === "en") {
+    return `Hello, ${name}!`
+} 
+    else if
+    (language === "es") {
+    return `¡Hola, ${name}!`
+} 
+    else if 
+    (language === "fr") {
+        return `Bonjour, ${name}!`
+    }
+    else if 
+    (language === "eo") {
+    return `Saluton, ${name}!`
+        }
+ 
+}
+
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+
+sum = function(arr) {
+    return arr.reduce(function(a,b){
+      return a + b
+    }, 0)
+  }
+
+
+
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(numbers) {
+    if (numbers.length > 0) {
+        let total = numbers.reduce(function(a, b) { return a + b}, 0)
+        return total / numbers.length
+}   else {
+}
+}
+
+
+
+
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+  function minimum(numbers) {
+    if (numbers.length > 0) {
+        let output = Math.min.apply(null, numbers)
+        return output
+}   else {
+}
+}
+
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
 // implement sorting ourselves, however.
-//
+//// array to sort
+
 // A "selection sort" is one of the most simple sorting algorithms. To implement it,
 // you start with an unsorted array of numbers. You search the array and find the
 // smallest number in the array. You then insert that into a new array as the first
